54e15ed2a5a53cb5165253bfe971a754f586d703
Donald Whitely <dswhitely1@gmail.com>
2019-06-18 15:39:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-// const cors = require('cors');
+const cors = require('cors');
 
 const users = require('./routes/api/users');
 
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(cors());
+app.use(cors());
 
 const db = process.env.DATABASE;
 
