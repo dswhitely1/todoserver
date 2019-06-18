c10b05f6326bb3b1805f5ba2331be4350c420506
Donald Whitely <dswhitely1@gmail.com>
2019-06-18 15:52:02 -0400

    Still working on cors

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const cors = require('cors');
 const users = require('./routes/api/users');
 
 const app = express();
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
